8e121ea3d526563358586404c4991979059f99c7
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-04 04:45:36 +0800

    update

--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -3,6 +3,5 @@ shopt -s extglob
 
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ipq60xx target/linux/ipq60xx
 
-git clone https://github.com/qosmio/nss-packages package/nss-packages
+svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/qca
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-dp kmod-qca-nss-drv kmod-qca-nss-drv-pppoe nss-firmware-ipq6018 qca-nss-ecm/' target/linux/ipq60xx/Makefile
--- devices/ipq806x_generic/diy.sh
+++ devices/ipq806x_generic/diy.sh
@@ -6,7 +6,7 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 git clone https://github.com/qosmio/nss-packages package/nss-packages
 
-rm -rf target/linux/ipq806x
-svn co https://github.com/APCCV/OpenWRT-23.05.0-rc1-NSS/trunk/target/linux/ipq806x target/linux/ipq806x
+#rm -rf target/linux/ipq806x
+#svn co https://github.com/APCCV/OpenWRT-23.05.0-rc1-NSS/trunk/target/linux/ipq806x target/linux/ipq806x
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-drv kmod-qca-nss-drv-qdisc kmod-qca-nss-ecm-standard kmod-qca-nss-gmac kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-nss-ifb qca-nss-crypto qca-nss-drv-igs/' target/linux/ipq806x/Makefile
+#sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-drv kmod-qca-nss-drv-qdisc kmod-qca-nss-ecm-standard kmod-qca-nss-gmac kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-nss-ifb qca-nss-crypto qca-nss-drv-igs/' target/linux/ipq806x/Makefile
