61f9deb795c61964decf9dfd76fac252575b8c85
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-03 15:55:47 +0800

    update

--- devices/ipq806x_generic/diy.sh
+++ devices/ipq806x_generic/diy.sh
@@ -6,4 +6,4 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 git clone https://github.com/qosmio/nss-packages package/nss-packages
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-drv kmod-qca-nss-ecm-standard kmod-qca-nss-gmac kmod-qca-nss-drv-pppoe kmod-qca-mcs/' target/linux/ipq806x/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-drv kmod-qca-nss-drv-qdisc kmod-qca-nss-ecm-standard kmod-qca-nss-gmac kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-nss-ifb qca-nss-crypto qca-nss-drv-igs/' target/linux/ipq806x/Makefile
